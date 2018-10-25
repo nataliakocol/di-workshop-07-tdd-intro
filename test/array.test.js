a734ea283eb7621ed6dc8f4234b68826d530e194
nataliakocol <natalia.kocol@ada.ac.uk>
2018-10-25 11:08:26 +0100

    finished the assignment

--- test/array.test.js
+++ test/array.test.js
@@ -5,15 +5,34 @@ var expect = chai.expect
 describe('Array', function() {
   describe('#push()', function() {
     it('should add an item to the array', function() {
-      var array = [1, 2, 3, 4]
-      array.push(5)
-      expect(array).to.deep.equal([1, 2, 3, 4, 5])
+      var array = [1, 2, 3, 4];
+      array.push(5);
+      expect(array).to.deep.equal([1, 2, 3, 4, 5]);
     })
 
-    it('should alter the length properly', function() {
-      var array = [1, 1, 1, 1]
-      array.push(1)
-      expect(array.length).to.equal(5)
+    it('should alter the length property', function() {
+      var array = [1, 1, 1, 1];
+      array.push(1);
+      expect(array.length).to.equal(5);
+    })
+  })
+  describe('#pop()', function() {
+    it('should remove an item from the array', function() {
+      var array = [1, 3, 4, 2];
+      array.pop();
+      expect(array).to.deep.equal([1, 3, 4]);
+    })
+    it('should alter the length property', function() {
+      var array = [1, 3, 4, 2];
+      array.pop();
+      expect(array.length).to.equal(3);
+    })
+  })
+  describe('#join()', function() {
+    it('should convert items from the array to a string', function() {
+      var array = ['1', '3', '4', '2'];
+      var x = array.join();
+      expect(x).to.equal('1,3,4,2');
     })
   })
 })
--- test/coolculator.test.js
+++ test/coolculator.test.js
@@ -1,25 +1,62 @@
-var chai = require('chai')
-var expect = chai.expect
+var chai = require('chai');
+var expect = chai.expect;
 
-var Coolculator = require('../lib/coolculator')
+var Coolculator = require('../lib/coolculator');
 
 describe('Coolculator', function() {
-  mm = new Coolculator()
+  mm = new Coolculator();
 
   it('should add', function() {
-    result = mm.add(2, 3)
-    expect(result).to.equal(5)
+    result = mm.add(2,3);
+    expect(result).to.equal(5);
   })
 
   // Uncomment this 👇
-  // it('should multiply', function() {
-  //   result = mm.multiply(2, 3)
-  //   expect(result).to.equal(6)
-  // })
+   it('should multiply', function() {
+     result = mm.multiply(2, 3);
+     expect(result).to.equal(6);
+   })
 
   // Uncomment this 👇
-  // it('should subtract', function() {
-  //   result = mm.subtract(3, 2)
-  //   expect(result).to.equal(1)
-  // })
+   it('should subtract', function() {
+     result = mm.subtract(3, 2);
+     expect(result).to.equal(1);
+   })
+
+   it('should divide', function() {
+     result = mm.divide(66, 11);
+     expect(result).to.equal(6);
+   })
+
+   it('should not divide by 0', function() {
+     result = mm.divide(3, 0);
+     expect(result).to.equal('you cannot divide by 0!');
+   })
+
+   it('should square', function() {
+     result = mm.square(9);
+     expect(result).to.equal(81);
+   })
+
+
+    it('should raise', function() {
+      result = mm.raise(2, 3);
+      expect(result).to.equal(8);
+    })
+
+    it('should work for isCool', function() {
+      result = mm.isCool(25352);
+      expect(result).to.equal(true);
+    })
+
+    it('should calculate a factorial', function() {
+      result = mm.factorial(4);
+      expect(result).to.equal(24);
+    })
+    it('should work for random', function() {
+      result = mm.random(67);
+      expect(result).to.be.below(67);
+      expect(result).to.be.above(0);
+    })
+
 })
